f53cd476d49676d2aeb0b122d762d89f03935650
U-METAL103\103 <ELY3M>
2024-04-10 02:02:57 -0500

    fixes

--- app/src/main/java/own/ownplanetsspherelivewallpaper/Constants.java
+++ app/src/main/java/own/ownplanetsspherelivewallpaper/Constants.java
@@ -7,6 +7,6 @@ import java.io.File;
 public class Constants {
 
     public static final String appPath = (Environment.getExternalStorageDirectory().getAbsolutePath() + "/PlanetsSphere/");
-    public static final String scorefile = "score.txt";
+    public static final String scorefile = "score_own.txt";
     public static final String scorefilepath = appPath+scorefile;
 }
